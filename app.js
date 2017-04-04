4f955c6514532b23ee6a345139e758e87855c2a1
sretundijr <sretundijr@gmail.com>
2017-04-04 14:29:05 -0600

    added the not unqiue functionality, add a work around for a bad regex

--- app.js
+++ app.js
@@ -6,19 +6,26 @@ $(function(){
         e.preventDefault();
         // alert("here");
         var userInput = $('.js-text-input').val();
-
+        // console.log(userInput);
+        // var userInputArr = userInput.split(" ");
+        // console.log(userInputArr);
         var subString = divideString(userInput);
         console.log(subString);
         var stringCount = totalWordCount(subString);
         console.log(stringCount);
         var avgWordLength = averageWordLength(subString);
         console.log(avgWordLength);
-        var lowerCaseArray = lowerCase(subString);
-        console.log(lowerCaseArray);
-    });
 
-    function divideString(userInput){
-        var subString = userInput.split(" ");
+        var filtered = wordsThatAreNotUnique(subString);
+        console.log(filtered);
+    });
+})
+
+function divideString(userInput){
+        var subString = userInput.toLowerCase()
+            .split(/[\s\r*,\(\)]+/);
+            //work around
+            subString.pop();
         return subString;
     }
 
@@ -39,11 +46,44 @@ $(function(){
         return avgWordLength;
     };
 
-    function lowerCase(stringArray){
-        var lowerCaseArray = [];
+    // function lowerCase(stringArray){
+    //     var lowerCaseArray = [];
+    //     for(var i = 0; i < stringArray.length; i++){
+    //         lowerCaseArray.push(stringArray[i].toLowerCase());
+    //     }
+    //     return lowerCaseArray;
+    // }
+
+    function wordsThatAreNotUnique(stringArray){
+        var notUnique = [
+            'to',
+            'the',
+            'and',
+            'on'
+        ];
+        console.log(notUnique);
+        // for(var i = 0; i < stringArray.length; i++){
+        //     var filteredArray = stringArray.find(function(notUnique){
+        //         return stringArray !== notUnique;
+        //     });
+        // }
+        // var isUnique;
+        var countUniqueWords = 0;
         for(var i = 0; i < stringArray.length; i++){
-            lowerCaseArray.push(stringArray[i].toLowerCase());
+            var counter = 0;
+            // isUnique = false;
+            for(var j = 0; j < notUnique.length; j++){
+                // console.log(stringArray[i]);
+                // console.log(notUnique[j])
+                if(stringArray[i] === notUnique[j]){
+                    counter++;
+                }
+            }
+            if(counter === 0){
+                // isUnique = true;
+                countUniqueWords++;
+            }
         }
-        return lowerCaseArray;
+        
+        return countUniqueWords;
     }
-})
