bde6061013975ba8bc18dec07192a1f9c7f77094
sretundijr <sretundijr@gmail.com>
2017-04-04 11:01:36 -0600

    built most of the functionality asked for, needs one more feature and refactor

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
 // your code here!
+//wait for page load
+$(function(){
+    $('.js-text-input-form').submit(function(e){
+        //prevents default form behavior
+        e.preventDefault();
+        // alert("here");
+        var userInput = $('.js-text-input').val();
+
+        var subString = divideString(userInput);
+        console.log(subString);
+        var stringCount = totalWordCount(subString);
+        console.log(stringCount);
+        var avgWordLength = averageWordLength(subString);
+        console.log(avgWordLength);
+    });
+
+    function divideString(userInput){
+        var subString = userInput.split(" ");
+        return subString;
+    }
+
+    function totalWordCount(stringArray){
+        return stringArray.length;
+    }
+
+    function averageWordLength(stringArray){
+        //declare locals to calc avg length
+        var currentWordLength = 0;
+        var avgWordLength = 0;
+        var total = 0;
+        //loop through the array of strings to calc avg
+        for(var i = 0; i < stringArray.length; i++){
+            total += stringArray[i].length;
+        }
+        avgWordLength = total / totalWordCount(stringArray);
+        return avgWordLength;
+    };
+})
